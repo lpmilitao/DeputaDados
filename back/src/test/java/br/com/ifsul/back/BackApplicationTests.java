cfc6f2e195abdf075a1c4354f3a336581843e7c2
Luiza Militão Pereira <luizamilitao>
2023-12-11 18:52:05 -0300

    adicionando corpo do projeto back e front

---
+++ back/src/test/java/br/com/ifsul/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifsul.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
